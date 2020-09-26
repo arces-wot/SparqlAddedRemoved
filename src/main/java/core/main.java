4e575b91b151af97e5d358bd5f2d3733ade2d364
Andrea Ferrari <andrea.ferrari24@studio.unibo.it>
2020-09-26 18:04:34 +0200

    lumb rdf-xml automatic convert failed (not well formed)

--- src/main/java/core/main.java
+++ src/main/java/core/main.java
@@ -1,10 +1,20 @@
 package core;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.glassfish.grizzly.http.io.OutputBuffer;
 
 import connector.IRequestFactory;
 import connector.ISparqlRequest;
 import connector.RequestFacotry;
 import connector.RequestFacotry.RequestName;
+import edu.lehigh.swat.bench.uba.Generator;
 import it.unibo.arces.wot.sepa.commons.response.QueryResponse;
 import it.unibo.arces.wot.sepa.commons.response.Response;
 import it.unibo.arces.wot.sepa.commons.response.UpdateResponse;
@@ -17,25 +27,32 @@ public class main {
 	public static void main (String[] args) {
 
 		 
-	    ISparqlRequest r=RequestFacotry.getIntance().getRequestByName(RequestName.SIMPLE_INSERT.toString());
-		process(r.execute());
+		//options:
+	//   -univ number of universities to generate; 1 by default
+	//   -index starting index of the universities; 0 by default
+	//   -seed seed used for random data generation; 0 by default
+	//   -daml generate DAML+OIL data; OWL data by default
+	//   -onto url of the univ-bench ontology
+		  
+	    int univNum = 2, startIndex = 0, seed = 0;
+	    boolean daml = false;
+	    String ontology = "http://lumb/for.sepa.test";
+
+	    HashMap<String,OutputStream> streams=new  HashMap<String,OutputStream>();
+	   
+	    new Generator().start(univNum, startIndex, seed, daml, ontology,streams);
+		System.out.print("streams.keySet().size(): "+ streams.keySet().size());
 		
-		ISparqlRequest q=RequestFacotry.getIntance().getRequestByName(RequestName.SIMPLE_QUERY.toString());
-//		process(q.execute());
+		Model model = ModelFactory.createDefaultModel() ;
+		//System.out.println(new String(((ByteArrayOutputStream )streams.values().iterator().next()).toByteArray()).substring(0,1000));
+		InputStream in = new ByteArrayInputStream(((ByteArrayOutputStream )streams.values().iterator().next()).toByteArray());
+		model.read(in,"RDF/XML");
 		
-		ISparqlRequest q2=RequestFacotry.getIntance().getRequestByName(RequestName.SIMPLE_QUERY.toString());
-//		process(q.execute());
-		
-		System.out.println("--->"+Inspector.areEq(((QueryResponse) q.execute()).getBindingsResults(),((QueryResponse) q2.execute()).getBindingsResults()));
-		
-		ISparqlRequest d=RequestFacotry.getIntance().getRequestByName(RequestName.SIMPLE_DELETE.toString());
-		process(d.execute());
-		
+		ByteArrayOutputStream temp = new ByteArrayOutputStream();
+		model.write(temp, "N-TRIPLE");
+
+		System.out.print( new String(temp.toByteArray()));
 	
-		
-		q=RequestFacotry.getIntance().getRequestByName(RequestName.SIMPLE_QUERY.toString());
-		process(q.execute());
-		
 	}
 	
 	public static void process(Response res) {
